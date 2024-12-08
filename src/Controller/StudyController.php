f943183f21061dfb7bedd7e4e05d442c2871376f
Antoine Guillo <guillo.antoine93@gmail.com>
2024-12-08 11:08:29 +0100

    list add update detail study

--- src/Controller/StudyController.php
+++ src/Controller/StudyController.php
@@ -2,17 +2,78 @@
 
 namespace App\Controller;
 
+use App\Entity\Study;
+use App\Form\StudyType;
+use App\Repository\StudyRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/studies', name: 'studies_')]
 class StudyController extends AbstractController
 {
-    #[Route('/study', name: 'app_study')]
-    public function index(): Response
+    #[Route('/', name: 'list')]
+    public function list(StudyRepository $studyRepository): Response
     {
+
+        $studies = $studyRepository->findAll();
+
         return $this->render('study/list.html.twig', [
-            'controller_name' => 'StudyController',
+            'studies' => $studies,
+        ]);
+    }
+
+    #[Route('/add', name: 'add', methods: ['GET', 'POST'])]
+    public function add(Request $request, EntityManagerInterface $manager)
+    {
+
+        $study = new Study();
+        $studyForm = $this->createForm(StudyType::class, $study);
+        $studyForm->handleRequest($request);
+
+        if ($studyForm->isSubmitted() && $studyForm->isValid()) {
+
+            $manager->persist($study);
+            $manager->flush();
+
+            $this->addFlash('success', 'Expérience professionnelle correctement ajoutée !');
+            return $this->redirectToRoute('job_list',
+//                ['id' => $study->getId()]
+            );
+        }
+
+        return $this->render('study/add.html.twig', [
+            'studyForm' => $studyForm
+        ]);
+    }
+
+    #[Route('/edit/{id}', name: 'edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Study $study, EntityManagerInterface $manager)
+    {
+        $studyForm = $this->createForm(StudyType::class, $study);
+        $studyForm->handleRequest($request);
+
+        if ($studyForm->isSubmitted() && $studyForm->isValid()) {
+            $manager->persist($study);
+            $manager->flush();
+            $this->addFlash("succes", "Le diplôme ou titre professionnel a bien été mis à jour !");
+            return $this->redirectToRoute('job_list');
+        }
+
+        return $this->render('study/edit.html.twig', [
+            'studyForm' => $studyForm,
+            'study' => $study
+        ]);
+    }
+
+    #[Route('/detail/{id}', name: 'detail', methods: ['GET', 'POST'])]
+    public function detail(Study $study)
+    {
+
+        return $this->render('study/detail.html.twig', [
+            'study' => $study
         ]);
     }
 }
--- src/Form/StudyType.php
+++ src/Form/StudyType.php
@@ -4,8 +4,12 @@ namespace App\Form;
 
 use App\Entity\Study;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Image;
 
 class StudyType extends AbstractType
 {
@@ -14,13 +18,22 @@ class StudyType extends AbstractType
         $builder
             ->add('degreeName')
             ->add('schoolName')
-            ->add('degreeState')
+            ->add('degreeState', ChoiceType::class, [
+                'choices' => [
+                    'En cours' => 'en cours',
+                    'Obtenu' => 'obtenu',
+                ],
+                'multiple' => false,
+                'expanded' => false
+            ])
             ->add('localization')
-            ->add('date', null, [
+            ->add('date', DateType::class, [
                 'widget' => 'single_text',
             ])
-            ->add('schoolPhoto')
-        ;
+            ->add('schoolPhoto', FileType::class, [
+                'mapped' => false,
+                'constraints' => [new Image(["maxSize" => '5M', 'maxSizeMessage' => 'Fichier trop volumineux',])],
+                'required' => false]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
