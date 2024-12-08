e034e17e5db45728771f2629eb51fca46f785e82
Antoine Guillo <guillo.antoine93@gmail.com>
2024-12-08 11:08:29 +0100

    list add update detail study

--- src/Entity/Study.php
+++ src/Entity/Study.php
@@ -5,6 +5,9 @@ namespace App\Entity;
 use App\Repository\StudyRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints\Choice;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 #[ORM\Entity(repositoryClass: StudyRepository::class)]
 class Study
@@ -15,18 +18,26 @@ class Study
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[NotBlank]
+    #[Length(max: 255, maxMessage: "Le nom du diplôme ou du titre' ne peux excéder 50 caratères")]
     private ?string $degreeName = null;
 
     #[ORM\Column(length: 100)]
+    #[NotBlank]
+    #[Length(max: 255, maxMessage: "Le nom de l'école ne peux excéder 50 caratères")]
     private ?string $schoolName = null;
 
     #[ORM\Column(length: 255)]
     private ?string $schoolLogo = null;
 
     #[ORM\Column(length: 50)]
+    #[NotBlank]
+    #[Choice(['En cours', 'Obtenu'])]
     private ?string $degreeState = null;
 
     #[ORM\Column(length: 50)]
+    #[NotBlank]
+    #[Length(max: 50, maxMessage: "Le nom de l'école ne peux excéder 50 caratères")]
     private ?string $localization = null;
 
     #[ORM\Column(type: Types::DATE_MUTABLE)]
