dfe192a28d4ea33429fc40d5be0b1f1aef96609c
Antoine Guillo <guillo.antoine93@gmail.com>
2025-05-07 23:11:45 +0200

    Intégration présentation + carrousel cursus universitaire

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\StudyRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,8 +10,12 @@ use Symfony\Component\Routing\Attribute\Route;
 class MainController extends AbstractController
 {
     #[Route('/', name: 'main_home')]
-    public function home(): Response
+    public function home(StudyRepository $studyRepository): Response
     {
-        return $this->render('main/home.html.twig');
+        $studies = $studyRepository->findBy([], ['date' => 'DESC']);
+
+        return $this->render('main/home.html.twig', [
+            'studies' => $studies
+        ]);
     }
 }
