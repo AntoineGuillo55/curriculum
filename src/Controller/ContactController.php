24fd467a5ea8c303720880dad806e63d17486bd2
Antoine Guillo <guillo.antoine93@gmail.com>
2025-06-18 22:52:41 +0200

    admin and contact

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -12,7 +12,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-
+  
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
